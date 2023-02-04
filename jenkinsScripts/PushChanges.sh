badf260268557e403e100f392d87fb96b8a4eb4f
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-04 16:58:35 +0100

    test7

--- jenkinsScripts/PushChanges.sh
+++ jenkinsScripts/PushChanges.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 COMMIT_MSG="Pipeline ejecutada por $1. Motivo: $2"
+ssh-keyscan -t rsa domain.example > ~/.ssh/known_hosts
 git config --global user.name "Auto commit"
 git config --global user.email "sergimicoortiz@gmail.com"
 git checkout -f master
