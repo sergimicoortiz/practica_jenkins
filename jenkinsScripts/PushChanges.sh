3af9032eaee660fc4af1f6b4163aaa4a5d5ff4d5
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-04 16:43:12 +0100

    test3

--- jenkinsScripts/PushChanges.sh
+++ jenkinsScripts/PushChanges.sh
@@ -2,10 +2,10 @@
 COMMIT_MSG="Pipeline ejecutada por $1. Motivo: $2"
 git config --global user.name "Auto commit"
 git config --global user.email "sergimicoortiz@gmail.com"
-# git add .
-# git commit -m "$COMMIT_MSG"
-# git push origin master
-git remote
-git branch -a
-git status
+git add .
+git commit -m "$COMMIT_MSG"
+git push
+# git remote
+# git branch -a
+# git status
 exit 0
