30df36c2e17078303591681e99cedc7d5af9bab8
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-04 16:48:16 +0100

    test5

--- jenkinsScripts/PushChanges.sh
+++ jenkinsScripts/PushChanges.sh
@@ -3,7 +3,6 @@ COMMIT_MSG="Pipeline ejecutada por $1. Motivo: $2"
 git config --global user.name "Auto commit"
 git config --global user.email "sergimicoortiz@gmail.com"
 git checkout master
-git pull
 git add .
 git commit -m "$COMMIT_MSG"
 git push origin master
