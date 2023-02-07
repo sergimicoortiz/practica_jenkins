374721cd52240160bf8ceb20821493e374a28759
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-07 19:26:51 +0100

    vercel test

--- jenkinsScripts/PushChanges.sh
+++ jenkinsScripts/PushChanges.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 COMMIT_MSG="Pipeline ejecutada por $1. Motivo: $2"
 ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
-git config --global user.name "Auto commit"
+git config --global user.name "$1"
 git config --global user.email "sergimicoortiz@gmail.com"
 git add .
 git commit -m "$COMMIT_MSG"
